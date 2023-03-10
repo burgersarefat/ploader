c34bca9c2730bef2e4f211f9229e8f599e5c4e7b
Murturtle <97775022+Murturtle@users.noreply.github.com>
2023-03-10 14:20:54 -0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -12,9 +12,9 @@ if (document.getElementById('sidemenu')) {
     sidemenu.innerHTML=`
     <input style="height:4%; border:solid black 1px; color:white; background:grey; font-family:Monospace;" id="urlinput"/>
     
-    <button style="height:4%; border:none; color:white; background:grey; font-family:Monospace;" onclick="url=document.getElementById('urlinput').value;if(url.startsWith('https://')){document.getElementById('sidemenuframe').src=url;}else if(url.startsWith('http://')){alert('[WARN] http might not work due to downgrade policy.');document.getElementById('sidemenuframe').src=url;}else{document.getElementById('sidemenuframe').src='https://'+url}">⌕</button>
+    <button style="width:20px; height:4%; border:none; color:white; background:grey; font-family:Monospace;" onclick="url=document.getElementById('urlinput').value;if(url.startsWith('https://')){document.getElementById('sidemenuframe').src=url;}else if(url.startsWith('http://')){alert('[WARN] http might not work due to downgrade policy.');document.getElementById('sidemenuframe').src=url;}else{document.getElementById('sidemenuframe').src='https://'+url}">⌕</button>
     
-    <button style="height:4%; border:none; color:white; background:grey; font-family:Monospace;" onclick="document.getElementById('sidemenuframe').src=document.getElementById('sidemenuframe').src;">&#x21bb;</button>
+    <button style="width:20px; height:4%; border:none; color:white; background:grey; font-family:Monospace;" onclick="document.getElementById('sidemenuframe').src=document.getElementById('sidemenuframe').src;">&#x21bb;</button>
     
     <button style="float:right; background:red; color:black; width:50px; height:4%; border:none; font-size:25px; font-family:Monospace;" onclick='sidemenu.setAttribute("style","display:none;");gui=false;'>-</button>
     
@@ -32,6 +32,5 @@ document.onkeydown = function (e) {
     }
   }
 }
-
 
 
