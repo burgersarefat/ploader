c19654bc9cb39bdcf781ccc06a8593ddb02549e9
burgersarefat <74564867+burgersarefat@users.noreply.github.com>
2023-03-15 19:32:54 -0700

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/Murturtle/ploader/main/loader.js").then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch("https://raw.githubusercontent.com/burgersarefat/ploader/main/loader.js").then(data=>{data.text().then(text=>{eval(text)})});
